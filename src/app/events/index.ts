e2683ff6346a8cbc5a4d24bfec7f35b09542deaf
7s.i <7s.i@pm.me>
2026-01-29 02:40:07 +0800

    feat(events): add index file for module exports of event classes

---
+++ src/app/events/index.ts
@@ -0,0 +1,15 @@
+export * from './domain-event.interface';
+
+// Module Exports
+export * from './permissions/permissions.events';
+export * from './documents/documents.events';
+export * from './tasks/tasks.events';
+export * from './daily/daily.events';
+export * from './quality/quality.events';
+export * from './acceptance/acceptance.events';
+export * from './issues/issues.events';
+export * from './overview/overview.events';
+export * from './members/members.events';
+export * from './audit/audit.events';
+export * from './calendar/calendar.events';
+export * from './settings/settings.events';
