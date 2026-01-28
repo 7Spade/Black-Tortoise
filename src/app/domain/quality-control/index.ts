e93ef2a79d253583660dc819011e163968a60bff
7s.i <7s.i@pm.me>
2026-01-29 00:12:04 +0800

    新增政策模組的導出：將 qc-validation.policy 加入質量控制模組的導出清單

--- src/app/domain/quality-control/index.ts
+++ src/app/domain/quality-control/index.ts
@@ -6,3 +6,4 @@ export * from './repositories/qc-item.repository.interface';
 export * from './value-objects/qc-item-id.vo';
 export * from './value-objects/qc-result.vo';
 export * from './value-objects/qc-status.vo';
+export * from './policies/qc-validation.policy';
